4448481453043bf39042bcd196df8a521ba9eb28
신동금 <eldhdpswl@gmail.com>
2024-04-03 18:06:32 +0900

    FirebaseAuthentication main commit

---
+++ app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.firebaseauthentication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaseauthentication.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        lateinit var auth: FirebaseAuth
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signIn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+            binding.userDetails.text = updateDate()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.userDetails.text = updateDate()
+    }
+
+    private fun updateDate(): String{
+        return "Email : ${auth.currentUser?.email}"
+    }
+
+}
